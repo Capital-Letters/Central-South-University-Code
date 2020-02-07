8abbd51b5ce2d3d6728c9b91ed1d776b66417436
fu <1204650012@qq.com>
2020-02-07 17:20:08 +0800

    add

---
+++ pipi1026.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int x1,y1,x2,y2;
+        int u1,v1,u2,v2;
+        cin >> x1 >> y1 >> x2 >> y2 ;
+        cin >> u1 >> v1 >> u2 >> v2 ;
+        double x,y;
+        
+    }
+}
