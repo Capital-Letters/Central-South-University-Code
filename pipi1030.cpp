535432b3c21edc5f6ddd08bf584deda51c9d84f0
fu <1204650012@qq.com>
2020-02-08 17:18:23 +0800

    add

---
+++ pipi1030.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        long long int m=(n*(n+1)/2)*(n*(n+1)/2);
+        cout << m << endl;
+    }
+}
