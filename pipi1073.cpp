151d0efbc82e0c193c61f2424f41da4c614a5246
fu <1204650012@qq.com>
2020-02-10 17:22:52 +0800

    add

---
+++ pipi1073.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m;
+    while(cin>>n>>m)
+    {
+        int A[n][m];
+        int dp[n][m];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                cin>>A[i][j];
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            
+        }
+    }
+}
