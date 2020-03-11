9797437eb7e6e47a3ea8d371f7d014dcd614c227
fu <1204650012@qq.com>
2020-03-11 21:56:34 +0800

    add

--- vanet.cc
+++ vanet.cc
@@ -71,7 +71,7 @@ int main(int argc , char *argv[])
     CommandLine cmd;
     cmd.AddValue("distance","Distance apart to place node (in meters).",distance);
     cmd.AddValue("nodeSpeed","Speed scalar of Nodes",nodeSpeed);
-    cmd.AddValue("numofNodes","Number of Nodes to sumulate",numofNodes);
+    cmd.AddValue("numOfNodes","Number of Nodes to sumulate",numOfNodes);
     cmd.AddValue("PacketSize","Packet size to send (in bytes).",PacketSize);
     cmd.AddValue("format","Format to use for data output.",format);
     cmd.AddValue("experiment","Identifier for experiment.",experiment);
@@ -80,6 +80,105 @@ int main(int argc , char *argv[])
     cmd.Parse(argc,argv);
 
     if(format!="omnet"&&format!="db")
+    {
+        NS_LOG_ERROR("Unknow outout format'"<< format <<"'");
+        return -1;
+    }
+    if(format == 'db')
+    {
+        NS_LOG_ERROR("sqlite support not compiled in.");
+        return -1;
+    }
+    {
+        stringstream sstr("");
+        sstr << double(numOfNodes);
+        input = sstr.str();
 
+    }
+
+    NS_LOG_INFO("Installing WiFi and Internet stack.");
+    WifiHelper wifi = WifiHelper::Default();
+    wifi.SetStandard(WIFI_PHY_STANDARD_80211p_CCH);
+    NqosWifiMacHelper wifiMac = NqosWifiMacHelper::Default();
+    wifiMac.SetType("ns3::AdhocWifiMac");
+    YansWifiPhyHelper wifiPhy =YansWifiPhyHelper::Default();
+    wifiPhy.Set("TxPowerEnd",DoubleValue(25));
+    wifiPhy.Set("TxPowerStart",DoubleValue(25));
+    wifiPhy.Set("EnergyDetectionThreshold",DoubleValue(-94));
+    wifiPhy.Set("CcaModelThreshold",DoubleValue(-94));
+
+    Ptr<YansWifiChannel> wifiChannel = CreateObject <YansWifiChannel>();
+
+    Ptr<ThreeLogDistancePropagationLossModel> log3=CreateObject<ThreeLogDistancePropagationLossModel>();
+    Ptr<NakagamiPropagationLossModel> nak=CreateObject<NakagamiProgationLossModel>();
+    log3->SetNext(nak);
+
+    wifiChannel->SetPropagationLossModel(log3);
+    wifiChannel->SetPropagationLossDelayModel(CreateObject<ConstantSpeedPropagationDelayModel>());
+    wifiPhy.SetChannel(wifiChannel);
+    NetDeviceContainer nodeDevices=wifi.Install(wifiphy,wifiMac,nodes);
+
+    InternetStackHelper internet;
+    internet.Install(nodes);
+    Ipv4AddressHelper ipAddrs;
+    ipAddrs.SetBase("192.168.0.0","255.255.255.0");
+    ipAddrs.Assign(nodeDevices);
+
+    MobilityHelper mobility;
+    mobility.SetPositionAllocator("ns3::RandomDiscPositionAllocator","X",StringValue("500.0"),"Y",StringValue("500.0"),"Rho",StringVaule("Uniform:0:200"));
+    mobility.InstallAll();
+
+    LogComponentEnable("Ns2MobilityHelper",LOG_LEVEL_DEBUG);
+
+    std::ofstream os;
+    os.open("movementLog.txt");
+
+    Config::Connect("/NodeList/1/$ns3::MobilityModel/CourseChange",MakeBoundCallback(&CourseChange,&os));
+
+    Ns2MobilityHelper ns2=Ns2MobilityHelper("mobility.txt");
+    ns2.Install();
+
+    Ptr<Nodes> tempNode[numOfNodes];
+    Ptr<Sender> sender[numOfNodes];
+    Ptr<Receiver> receiver[numOfNodes];
+
+    UniformVariable uVar(0,0.001);
+
+    for(int ni=0;ni <numOfNodes;ni++)
+    {
+        tempNode[ni]=NodeList::GetNode(ni);
+        sender[ni]=CreateObject<Sender>();
+        sender[ni]->SetStartTime(Seconds(uVar.GetValue()));
+        tempNode[ni]->AddApplication(receiver[ni]);
+    }
+    NS_LOG_INFO("Finished installing Apps on each node");
+    Config::Set("/NodeList/*?ApplicationList/0/$Sender/PacketSize",UintegerValue(PacketSize));
+    Config::Set("/NodeList/*ApplicationList/0/$Sender/Destination",Ipv4AddressValue("255.255.255.255"));
+
+    DataCollector data;
+    data.DescribeRun(
+        experiment,
+        strategy,input,
+        runID);
+    data.AddMetadata("author","ensc427");
+    Ptr<CounterCalculator<uint32_t>>();
+    totalTx->SetKey("wifi-tx-Frames");
+    totalTx->SetContext("node[0]");
+    Config::Connect("/NodeList/0/DeviceList/*/$ns3::WifiNetDevice/Mac/MacTx",MakeBoundCallback(&TxCallback,totalTx));
+    data.AddDataCalculator(totalTx);
+
+    Ptr<PacketCounterCalculator> totalRx=CreateObject<PacketCounterCalculator>();
+    totalRx->SetKey("wifi-rx-Frames");
+    totalRx->SetContext("node[1]");
+    Config::Connect("/NodeList/1/DeviceList/*/$ns3::WifiNetDevice/Mac/MacRx",MakeCallback(&PacketCounterCalculator::PacketUpdate,totalRx));
+    data.AddDataCalculator(totalRx);
+
+    Ptr<PacketCounterCalculator> appTx = CreateObject<PacketCounterCalculator>();
+    appTx->SetKey("sender-tx-packets");
+    appTx->SetContext("node[0]");
+    Config::Connect("/NodeList/0/ApplicationList/0/$ender/Tx",MakeCallback(&PacketCounterCalculator::PacketUpdate,appTx));
+    data.AddDataCalculator(appTx);
+
+    Ptr<CounterCalculator<>> appRx= CreateObject<CounterCalculator>();
 
 }
