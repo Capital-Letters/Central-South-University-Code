bfff531b41a2a03919b7e11f49e2ffeb1c5ee50a
fu <1204650012@qq.com>
2020-03-15 21:11:41 +0800

    add

---
+++ Cat.java
@@ -0,0 +1,26 @@
+package com.imooc.animal;
+/**
+ * 宠物猫类
+ * @author imooc
+ *
+ */
+public class Cat {
+	String name;
+	int month;
+	double weight;
+	String species;
+	
+	public Cat() {
+		System.out.println("test");
+	}
+	
+	public void run()
+	{
+		System.out.println("run");
+	}
+	
+	public void eat()
+	{
+		System.out.println("eat");
+	}
+}
