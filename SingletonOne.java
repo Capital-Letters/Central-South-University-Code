c6a84dbb5717af1b9f91f14bc17bab7d704b1e11
fu <1204650012@qq.com>
2020-03-20 22:59:05 +0800

    add

---
+++ SingletonOne.java
@@ -0,0 +1,13 @@
+package com.imooc.singleton;
+
+public class SingletonOne {
+	private SingletonOne() {
+		
+	}
+	
+	private static SingletonOne instance=new SingletonOne();
+	
+	public static SingletonOne getInstance() {
+		return instance;
+	}
+}
